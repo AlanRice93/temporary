788eb98071b9e7a47df9c9158f2065c9fdc2aae3
Dax Ravi <d@ironbay.co>
2020-03-10 17:52:19 -0400

    Update types

--- packages/javascript/src/types.ts
+++ packages/javascript/src/types.ts
@@ -1,6 +1,9 @@
-interface Mutation {
-    merge: { [key: string]: any | Mutation['merge'] }
-    delete: { [key: string]: 1 | Mutation['delete'] }
-}
+declare namespace Riptide {
+    interface Mutation {
+        merge: { [key: string]: any | Mutation['merge'] }
+        delete: { [key: string]: 1 | Mutation['delete'] }
+    }
 
-type Query = { [key: string]: Query | { min?: string, max?: string, limit?: string } }
+    type Query = { [key: string]: Query | { min?: string, max?: string, limit?: string } }
+
+}
